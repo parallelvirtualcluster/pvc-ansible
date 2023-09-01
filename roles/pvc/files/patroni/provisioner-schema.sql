da24aaf5ff4278a0e48148c8c97a5054b16a6b5b
Joshua M. Boniface <joshua@boniface.me>
2023-09-01 15:42:22 -0400

    Install Provisioner schema to database

--- roles/pvc/files/patroni/provisioner-schema.sql
+++ roles/pvc/files/patroni/provisioner-schema.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE pvcprov WITH owner = pvcprov CONNECTION LIMIT = -1;
-\c pvcprov
-CREATE TABLE system_template (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE, vcpu_count INT NOT NULL, vram_mb INT NOT NULL, serial BOOL NOT NULL, vnc BOOL NOT NULL, vnc_bind TEXT, node_limit TEXT, node_selector TEXT, start_with_node BOOL NOT NULL);
+CREATE TABLE system_template (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE, vcpu_count INT NOT NULL, vram_mb INT NOT NULL, serial BOOL NOT NULL, vnc BOOL NOT NULL, vnc_bind TEXT, node_limit TEXT, node_selector TEXT, node_autostart BOOL NOT NULL);
 CREATE TABLE network_template (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE, mac_template TEXT);
 CREATE TABLE network (id SERIAL PRIMARY KEY, network_template INT REFERENCES network_template(id), vni INT NOT NULL);
 CREATE TABLE storage_template (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE);
@@ -8,8 +6,5 @@ CREATE TABLE storage (id SERIAL PRIMARY KEY, storage_template INT REFERENCES sto
 CREATE TABLE userdata_template (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE, userdata TEXT NOT NULL);
 CREATE TABLE script (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE, script TEXT NOT NULL);
 CREATE TABLE profile (id SERIAL PRIMARY KEY, name TEXT NOT NULL UNIQUE, system_template INT REFERENCES system_template(id), network_template INT REFERENCES network_template(id), storage_template INT REFERENCES storage_template(id), userdata_template INT REFERENCES userdata_template(id), script INT REFERENCES script(id), arguments text);
-GRANT ALL PRIVILEGES ON DATABASE pvcprov TO pvcprov;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO pvcprov;
-GRANT ALL PRIVILEGES ON ALL SQQUENCES IN SCHEMA public TO pvcprov;
 
 INSERT INTO userdata_template(name, userdata) VALUES ('empty', '');
