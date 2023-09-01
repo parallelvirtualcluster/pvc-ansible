7dc6efdf9a1aecb06d4244ef8a9b1b5d4ce1ea7f
Joshua M. Boniface <joshua@boniface.me>
2023-09-01 15:42:23 -0400

    Add update to purge command

--- purge.sh
+++ purge.sh
@@ -10,6 +10,7 @@ for host in $@; do
     sudo apt purge -y ca-certificates-java fontconfig-config libjemalloc2 libpq5 python-psycopg2 python3-eventlet python3-greenlet python3-jinja2 python3-kazoo python3-markupsafe python3-pkg-resources python3-pygments python3-six uuid-runtime
     sudo apt autoremove --purge -y;
     sudo apt clean;
+    sudo apt update;
     sudo rm -rf /etc/pvc-install.* /etc/ceph* /etc/patroni* /etc/postgres* /etc/zookeeper* /etc/libvirt*;
     sudo rm -rf /var/lib/postgresql /var/lib/zookeeper /var/lib/libvirt;
     sudo rm -rf /run/ceph;
